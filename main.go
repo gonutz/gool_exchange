75eac8a0d60486062a834a8475eac156340170cc
Lars Krumwiede <lars.krumwiede@kgsschwarmstedt.de>
2024-11-27 14:47:50 +0100

    Update main.go

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
     "fmt"
     "net"
+	"strings"
 )
 
 func main() {
+	myAddress := ""
+
     addrs, err := net.InterfaceAddrs()
     if err != nil {
         fmt.Println("Error:", err)
@@ -28,5 +31,11 @@ func main() {
         if ipNet.IP.To4() != nil {
             fmt.Println("Local IP address:", ipNet.IP.String())
         }
+
+		if strings.HasPrefix(ipNet.IP.String(), "10.") {
+			myAddress = ipNet.IP.String()
+		}
     }
+
+	fmt.Println("my IP is", myAddress)
 }
