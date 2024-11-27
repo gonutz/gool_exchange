94ba506272df19592a5fdba42eff626b0a1f3788
Lars Krumwiede <lars.krumwiede@kgsschwarmstedt.de>
2024-11-27 14:48:59 +0100

    Update main.go

--- main.go
+++ main.go
@@ -7,12 +7,17 @@ import (
 )
 
 func main() {
+	if err := run(); err != nil {
+		panic(err)
+	}
+}
+
+func run() error {
 	myAddress := ""
 
     addrs, err := net.InterfaceAddrs()
     if err != nil {
-        fmt.Println("Error:", err)
-        return
+        return err
     }
 
     for _, addr := range addrs {
@@ -27,11 +32,11 @@ func main() {
             continue
         }
 
-
 		if strings.HasPrefix(ipNet.IP.String(), "10.") {
 			myAddress = ipNet.IP.String()
 		}
     }
 
 	fmt.Println("my IP is", myAddress)
+	return nil
 }
