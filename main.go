c3459cc76ab076e3abb9cf942d560fe90cce18ca
Lars Krumwiede <lars.krumwiede@kgsschwarmstedt.de>
2024-11-27 14:57:27 +0100

    Update main.go

--- main.go
+++ main.go
@@ -2,8 +2,13 @@ package main
 
 import (
     "fmt"
+	"time"
     "net"
 	"strings"
+)
+
+const (
+	pingPort = "25468"
 )
 
 func main() {
@@ -37,6 +42,55 @@ func run() error {
 		}
     }
 
+	if myAddress == "" {
+		return fmt.Errorf("own IP address not found")
+	}
+
 	fmt.Println("my IP is", myAddress)
+
+    broadcastAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:" + pingPort)
+    if err != nil {
+		return err
+    }
+
+    conn, err := net.DialUDP("udp", nil, broadcastAddr)
+    if err != nil {
+		return err
+    }
+    defer conn.Close()
+
+	msg := "Hello Network!"
+    _, err = conn.Write([]byte(msg))
+    if err != nil {
+		return err
+    }
+
+    listenAddr, err := net.ResolveUDPAddr("udp", ":" + pingPort)
+    if err != nil {
+		return err
+    }
+
+    listenConn, err := net.ListenUDP("udp", listenAddr)
+    if err != nil {
+		return err
+    }
+    defer listenConn.Close()
+
+	go func() {
+		buffer := make([]byte, 1024)
+
+		for {
+	        n, addr, err := listenConn.ReadFromUDP(buffer)
+	        if err != nil {
+	            fmt.Println("Error receiving data:", err)
+	            return
+	        }
+
+    	    fmt.Printf("Received message from %s: %s\n", addr, string(buffer[:n]))
+    	}
+	}()
+
+	time.Sleep(5 * time.Second)
+
 	return nil
 }
