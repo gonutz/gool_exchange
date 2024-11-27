1f3ee5358d4642230c14a1083f7b2486ee7c2fcc
Lars Krumwiede <lars.krumwiede@kgsschwarmstedt.de>
2024-11-27 15:09:19 +0100

    Update main.go

--- main.go
+++ main.go
@@ -49,16 +49,23 @@ func run() error {
 
 	fmt.Println("my IP is", myAddress)
 
-    broadcastAddr, err := net.ResolveUDPAddr("udp", broadcastIP + ":" + pingPort)
+	localAddr, err := net.ResolveUDPAddr("udp", myAddress+":0")
     if err != nil {
 		return err
     }
 
-    conn, err := net.DialUDP("udp", nil, broadcastAddr)
+    broadcastAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:" + pingPort)
+    if err != nil {
+		return err
+    }
+
+    conn, err := net.DialUDP("udp", localAddr, broadcastAddr)
     if err != nil {
 		return err
     }
     defer conn.Close()
+
+	// TODO
 
 	msg := "Hello Network!"
     _, err = conn.Write([]byte(msg))
@@ -95,3 +102,70 @@ func run() error {
 
 	return nil
 }
+
+
+
+
+
+/*package main
+
+import (
+    "net"
+    "fmt"
+    "os"
+    "syscall"
+)
+
+func main() {
+    // Local IP address of the specific network interface
+    localIP := "192.168.1.100" // Change this to your specific local IP address
+
+    // Create a local UDP address for the specific interface
+    localAddr, err := net.ResolveUDPAddr("udp", localIP+":0")
+    if err != nil {
+        fmt.Println("Error resolving local address:", err)
+        os.Exit(1)
+    }
+
+    // Broadcast address and port
+    broadcastAddr, err := net.ResolveUDPAddr("udp", "255.255.255.255:12345")
+    if err != nil {
+        fmt.Println("Error resolving broadcast address:", err)
+        os.Exit(1)
+    }
+
+    // Create a UDP connection bound to the specific interface
+    conn, err := net.DialUDP("udp", localAddr, broadcastAddr)
+    if err != nil {
+        fmt.Println("Error creating connection:", err)
+        os.Exit(1)
+    }
+    defer conn.Close()
+
+    // Set the socket option to enable broadcasting
+    f, err := conn.File()
+    if err != nil {
+        fmt.Println("Error getting file descriptor:", err)
+        os.Exit(1)
+    }
+    defer f.Close()
+
+    if err := syscall.SetsockoptInt(int(f.Fd()), syscall.SOL_SOCKET, syscall.SO_BROADCAST, 1); err != nil {
+        fmt.Println("Error setting socket option:", err)
+        os.Exit(1)
+    }
+
+    // Data to broadcast
+    message := []byte("Hello, UDP Broadcast!")
+
+    // Send the data
+    _, err = conn.Write(message)
+    if err != nil {
+        fmt.Println("Error sending data:", err)
+        os.Exit(1)
+    }
+
+    fmt.Println("Broadcast message sent successfully on specific interface!")
+}
+
+*/
