12c23042e8edef7f75f7f83e4be8e821f0548157
Lars Krumwiede <lars.krumwiede@kgsschwarmstedt.de>
2024-11-27 14:43:48 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+    "fmt"
+    "net"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+    addrs, err := net.InterfaceAddrs()
+    if err != nil {
+        fmt.Println("Error:", err)
+        return
+    }
+
+    for _, addr := range addrs {
+        // Check if the address is an IP address
+        ipNet, ok := addr.(*net.IPNet)
+        if !ok {
+            continue
+        }
+
+        // Skip loopback addresses
+        if ipNet.IP.IsLoopback() {
+            continue
+        }
+
+        // Check if the address is an IPv4 address
+        if ipNet.IP.To4() != nil {
+            fmt.Println("Local IP address:", ipNet.IP.String())
+        }
+    }
 }
